25998dfaaf721621396a267a64a3e4bbeed43a1f
TheofilosGeo <theofilos.gewrgakis@gmail.com>
2023-03-22 22:25:59 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,40 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "tmp") {
         value = Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C)
-        radio.sendNumber(value)
     } else if (receivedString == "hum") {
         value = Environment.octopus_BME280(Environment.BME280_state.BME280_humidity)
-        radio.sendNumber(value)
     } else if (receivedString == "hpa") {
         value = Environment.octopus_BME280(Environment.BME280_state.BME280_pressure)
-        radio.sendNumber(value)
     } else if (receivedString == "umv") {
         value = Environment.UVLevel(AnalogPin.P4)
-        radio.sendNumber(value)
     } else if (receivedString == "dbm") {
         value = Environment.ReadNoise(AnalogPin.P1)
-        radio.sendNumber(value)
     } else if (receivedString == "lvl") {
-    	
+        TankDistance = sonar.ping(
+        DigitalPin.P15,
+        DigitalPin.P14,
+        PingUnit.Centimeters
+        )
+        while (TankDistance == 0) {
+            TankDistance = sonar.ping(
+            DigitalPin.P15,
+            DigitalPin.P14,
+            PingUnit.Centimeters
+            )
+        }
+        if (TankDistance <= 4) {
+            value = 1
+        } else if (TankDistance <= 7) {
+            value = 2
+        } else {
+            value = 3
+        }
     } else if (receivedString == "rnp") {
         value = pins.analogReadPin(AnalogPin.P0) * 3.3 / 1023
-        radio.sendNumber(value)
     }
+    radio.sendNumber(value)
 })
+let TankDistance = 0
 let value = 0
 basic.showIcon(IconNames.Yes)
 radio.setGroup(167)
